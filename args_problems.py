a979672f70ee720e6ef1718e17843345c039f054
Adrian Salazar <113551883+asalazar23@users.noreply.github.com>
2022-11-14 21:20:58 +0000

    challenges

--- args_problems.py
+++ args_problems.py
@@ -1,22 +1,22 @@
-###############################################################################################
+
+####7##7#/########################################################################################
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
 # For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
 
-def sum_square(*args):
-  sum = 0
-  return sum += args
-
+def sum_squares(*args):
+  return sum(args)
+print(sum_squares(1,2,3))
 
 ###############################################################################################
 # Indefinite Arguments (*args) Practice #2
 # Create a function called absolute_sum, which takes any number of arguments, and returns the sum of their absolute values (that is, it takes the non-negative values and adds them together, in other words, considers them all - negative and positive - as positive).
-
-def absolute_sum():
-  
-  
-
+import math
+def absolute_sum(*args):
+  for i in args:
+    math.fabs(i)
+  return sum(args)
 
 ###############################################################################################
 # Indefinite Arguments (*args) Practice #3
@@ -26,10 +26,9 @@ def absolute_sum():
 
 # "{name}, the sum of your numbers is {sum_numbers}"
 
-
 def personal_numbers(name,*args):
+  sum_numbers = 0
+  sum_numbers +=  args
   print(f"{name}, the sum of your numbers is {sum_numbers}")
 
-  hackers in movies be like
-
-  uiasdhfghs dkjf ahkaghbcuityureith yasrgfyawrefg saggjadsgfufgygfgasdfuyaejgfhvsgh    
+ 
